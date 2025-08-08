5260a79e38874a025f1bfab57bcd127fc44c3e33
Uncodie <sergio@uncodie.com>
2025-08-07 18:21:32 -0600

    fix: resolve TypeScript build errors with D3 types

--- next-env.d.ts
+++ next-env.d.ts
@@ -2,4 +2,15 @@
 /// <reference types="next/image-types/global" />
 
 // NOTE: This file should not be edited
-// see https://nextjs.org/docs/pages/building-your-application/configuring/typescript for more information.
+// see https://nextjs.org/docs/basic-features/typescript for more information.
+
+// Suppress problematic D3 types that cause build issues
+declare module '@types/d3-dispatch' {
+  const content: any;
+  export = content;
+}
+
+declare module 'd3-dispatch' {
+  const content: any;
+  export = content;
+}
